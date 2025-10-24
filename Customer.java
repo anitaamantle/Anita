5a11e3cb8d69143107a5f0f79974ff2ff84fbe1b
anitaamantle <cse24-075@thuto.bac.ac.bw>
2025-10-24 20:22:03 +0200

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -5,17 +5,19 @@ public abstract class Customer {
 
     private String customerID;
     private String customerName;
+    private String contactNumber;
+    private String email;
+    private String dateOfBirth;
     private List<Account> accounts; 
 
-
-    public Customer(String customerID, String customerName) {
+    public Customer(String customerID, String customerName, String contactNumber, String email, String dateOfBirth) {
         this.customerID = customerID;
         this.customerName = customerName;
+        this.contactNumber = contactNumber;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
         this.accounts = new ArrayList<>();
     }
-
-
-
 
     public void openAccount(Account account) {
         if (account != null) {
@@ -24,15 +26,13 @@ public abstract class Customer {
         }
     }
 
-    public String getCustomerID() {
-        return customerID;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public List<Account> getAccounts() {
-        return accounts;
-    }
+    public String getCustomerID() { return customerID; }
+    public String getCustomerName() { return customerName; }
+    public String getContactNumber() { return contactNumber; }
+    public String getEmail() { return email; }
+    public String getDateOfBirth() { return dateOfBirth; }
+    public List<Account> getAccounts() { return accounts; }
+    
+    public void setContactNumber(String contactNumber) { this.contactNumber = contactNumber; }
+    public void setEmail(String email) { this.email = email; }
 }
