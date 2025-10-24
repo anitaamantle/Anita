844e69eb6f718d71978d28694d790c2646e4991c
anitaamantle <cse24-075@thuto.bac.ac.bw>
2025-10-24 20:23:05 +0200

    Update IndividualCustomer.java

--- IndividualCustomer.java
+++ IndividualCustomer.java
@@ -2,23 +2,26 @@ public class Individual extends Customer {
 
     private String firstName;
     private String lastName;
+    private String maritalStatus;
+    private String sourceOfFunds;
+    private String employer;
 
-    // Constructor
-    public Individual(String customerID, String firstName, String lastName) {
-        // Concatenate names for the base class (assuming CustomerName is full name)
-        super(customerID, firstName + " " + lastName);
+    public Individual(String customerID, String firstName, String lastName, String contactNumber, String email, String dateOfBirth, String maritalStatus, String sourceOfFunds, String employer) {
+        super(customerID, firstName + " " + lastName, contactNumber, email, dateOfBirth);
         this.firstName = firstName;
         this.lastName = lastName;
+        this.maritalStatus = maritalStatus;
+        this.sourceOfFunds = sourceOfFunds;
+        this.employer = employer;
     }
 
-
-
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getMaritalStatus() { return maritalStatus; }
+    public String getSourceOfFunds() { return sourceOfFunds; }
+    public String getEmployer() { return employer; }
     
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
+    public void setMaritalStatus(String maritalStatus) { this.maritalStatus = maritalStatus; }
+    public void setSourceOfFunds(String sourceOfFunds) { this.sourceOfFunds = sourceOfFunds; }
+    public void setEmployer(String employer) { this.employer = employer; }
 }
