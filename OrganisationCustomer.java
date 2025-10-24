082c563ed8167a0b1c60f09c322388ec920f9459
anitaamantle <cse24-075@thuto.bac.ac.bw>
2025-10-24 20:24:54 +0200

    Update OrganisationCustomer.java

--- OrganisationCustomer.java
+++ OrganisationCustomer.java
@@ -1,23 +1,17 @@
 public class Organisation extends Customer {
 
-
     private String organisationName;
     private String registrationNumber;
 
-
-
-    public Organisation(String customerID, String organisationName, String registrationNumber) {
-        // Use organisationName for the base class CustomerName
-        super(customerID, organisationName);
+    public Organisation(String customerID, String organisationName, String registrationNumber, String contactNumber, String email, String dateOfEstablishment) {
+        super(customerID, organisationName, contactNumber, email, dateOfEstablishment); 
         this.organisationName = organisationName;
         this.registrationNumber = registrationNumber;
     }
 
-    public String getOrganisationName() {
-        return organisationName;
-    }
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
-    }
+    public String getOrganisationName() { return organisationName; }
+    public String getRegistrationNumber() { return registrationNumber; }
+    
+    public void setOrganisationName(String organisationName) { this.organisationName = organisationName; }
+    public void setRegistrationNumber(String registrationNumber) { this.registrationNumber = registrationNumber; }
 }
